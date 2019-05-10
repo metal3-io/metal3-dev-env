374954af54c0b91f13a2133983e9357b0d0dd1c9
Russell Bryant <rbryant@redhat.com>
2019-05-10 12:02:06 -0400

    Factor out user data secret generation into a script.

--- provision_host.sh
+++ provision_host.sh
@@ -12,56 +12,12 @@ if [ -z "${BMHOST}" ] ; then
     exit 1
 fi
 
-#
-# Our virtual bare metal environment is created with two networks: NIC 1)
-# "provisioning" NIC 2) "baremetal"
-#
-# cloud-init based images will only bring up the first network interface by
-# default.  We need it to bring up our second interface, as well.
-#
-# TODO(russellb) - It would be nice to make this more dynamic and also not
-# platform specific.  cloud-init knows how to read a network_data.json file
-# from config drive.  Maybe we could have the baremetal-operator automatically
-# generate a network_data.json file that says to do DHCP on all interfaces that
-# we know about from introspection.
-#
-network_config_files() {
-    if echo ${IMAGE_NAME} | grep -qi centos 2>/dev/null ; then
-cat << EOF
-write_files:
-- path: /etc/sysconfig/network-scripts/ifcfg-eth1
-  owner: root:root
-  permissions: '0644'
-  content: |
-    BOOTPROTO=dhcp
-    DEVICE=eth1
-    ONBOOT=yes
-    TYPE=Ethernet
-    USERCTL=no
-runcmd:
- - [ ifup, eth1 ]
-EOF
-    fi
-}
-
-user_data_secret() {
-    printf "#cloud-config\n\nssh_authorized_keys:\n  - " > .userdata.tmp
-    cat ${SSH_PUB_KEY} >> .userdata.tmp
-    printf "\n" >> .userdata.tmp
-    network_config_files >> .userdata.tmp
-cat << EOF
-apiVersion: v1
-data:
-  userData: $(base64 -w 0 .userdata.tmp)
-kind: Secret
-metadata:
-  name: ${BMHOST}-user-data
-  namespace: metal3
-type: Opaque
-EOF
-rm .userdata.tmp
-}
-user_data_secret | kubectl apply -n metal3 -f -
+if echo ${IMAGE_NAME} | grep -qi centos 2>/dev/null ; then
+    OS_TYPE=centos
+else
+    OS_TYPE=unknown
+fi
+user_data.sh ${BMHOST} ${OS_TYPE} | kubectl apply -n metal3 -f -
 
 kubectl patch baremetalhost ${BMHOST} -n metal3 --type merge \
     -p '{"spec":{"image":{"url":"'${IMAGE_URL}'","checksum":"'${IMAGE_CHECKSUM}'"},"userData":{"name":"'${BMHOST}'-user-data","namespace":"metal3"}}}'
---
+++ user_data.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+SECRET_NAME_PREFIX=$1
+OS_TYPE=${2:-unknown}
+
+if [ -z "${SECRET_NAME_PREFIX}" ] || [ -z "${SSH_PUB_KEY}" ] ; then
+    echo "Usage: user_data.sh <secret name prefix> [os type]"
+    echo
+    echo '    os type: "centos", or "unknown" (default)'
+    echo
+    echo 'Expected env vars:'
+    echo '    SSH_PUB_KEY - path to ssh public key'
+    exit 1
+fi
+
+#
+# Our virtual bare metal environment is created with two networks: NIC 1)
+# "provisioning" NIC 2) "baremetal"
+#
+# cloud-init based images will only bring up the first network interface by
+# default.  We need it to bring up our second interface, as well.
+#
+# TODO(russellb) - It would be nice to make this more dynamic and also not
+# platform specific.  cloud-init knows how to read a network_data.json file
+# from config drive.  Maybe we could have the baremetal-operator automatically
+# generate a network_data.json file that says to do DHCP on all interfaces that
+# we know about from introspection.
+#
+network_config_files() {
+    if [ "$OS_TYPE" = "centos" ] ; then
+cat << EOF
+write_files:
+- path: /etc/sysconfig/network-scripts/ifcfg-eth1
+  owner: root:root
+  permissions: '0644'
+  content: |
+    BOOTPROTO=dhcp
+    DEVICE=eth1
+    ONBOOT=yes
+    TYPE=Ethernet
+    USERCTL=no
+runcmd:
+ - [ ifup, eth1 ]
+EOF
+    fi
+}
+
+user_data_secret() {
+    printf "#cloud-config\n\nssh_authorized_keys:\n  - " > .userdata.tmp
+    cat ${SSH_PUB_KEY} >> .userdata.tmp
+    printf "\n" >> .userdata.tmp
+    network_config_files >> .userdata.tmp
+cat << EOF
+apiVersion: v1
+data:
+  userData: $(base64 -w 0 .userdata.tmp)
+kind: Secret
+metadata:
+  name: ${SECRET_NAME_PREFIX}-user-data
+  namespace: metal3
+type: Opaque
+EOF
+rm .userdata.tmp
+}
+
+user_data_secret
