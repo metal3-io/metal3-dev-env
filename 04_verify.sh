6a10063a273bd63a4952af03a43300900ee718f7
maelk <mael.kimmerlin@est.tech>
2019-08-13 12:55:03 +0300

    Add retries in verification script
    Avoid failing without retrying to wait for the dev env
    to initialize properly. Add verify target in Makefile all

--- 04_verify.sh
+++ 04_verify.sh
@@ -11,15 +11,43 @@ source lib/logging.sh
 # shellcheck disable=SC1091
 source lib/common.sh
 
+SKIP_RETRIES=false
+
+TEST_TIME_INTERVAL=10
+TEST_MAX_TIME=60
+
+iterate(){
+  local RUNS=0
+  # shellcheck disable=SC2068
+  TMP_FAILS="$($@)"
+  TMP_RET="$?"
+  until [[ "${TMP_RET}" == 0 ]] || [[ "${SKIP_RETRIES}" == true ]]
+  do
+    RUNS="$((RUNS+1))"
+    if [[ "${RUNS}" == "${TEST_MAX_TIME}" ]]; then
+      break
+    fi
+    echo -e "\n================\nErrors, retrying\n================" >&3
+    sleep "${TEST_TIME_INTERVAL}"
+    # shellcheck disable=SC2068
+    TMP_FAILS="$($@)"
+    TMP_RET="$?"
+  done
+  echo "${TMP_FAILS}"
+  return "${TMP_RET}"
+}
+
 
 # Check the return code and log
 process_status(){
   if [[ "${1}" == 0 ]]; then
     echo "OK - ${2}" >&3
     echo "${FAILS}"
+    return 0
   else
     echo "FAIL - ${2}" >&3
     echo "$((FAILS+1))"
+    return 1
   fi
 }
 
@@ -29,9 +57,11 @@ equals(){
   if [[ "${1}" == "${2}" ]]; then
     echo "OK - ${3}" >&3
     echo "${FAILS}"
+    return 0
   else
     echo "FAIL - ${3}" >&3
     echo "$((FAILS+1))"
+    return 1
   fi
 }
 
@@ -41,14 +71,19 @@ differs(){
   if [[ "${1}" != "${2}" ]]; then
     echo "OK - ${3}" >&3
     echo "${FAILS}"
+    return 0
   else
     echo "FAIL - ${3}" >&3
     echo "$((FAILS+1))"
+    return 1
   fi
 }
 
 
 check_bm_hosts() {
+    local FAILS_CHECK="${FAILS}"
+    BM_HOSTS="$(kubectl --kubeconfig "${KUBECONFIG}" get baremetalhosts -n metal3 -o json)"
+    BM_VMS="$(virsh list --all)"
     while read -r name address user password mac; do
       BM_VMNAME="${name//-/_}"
       # Verify BM host exists
@@ -85,20 +120,30 @@ check_bm_hosts() {
       echo "$BM_VMS "| grep -w "${BM_VMNAME}"  > /dev/null
       FAILS="$(process_status $? "${name} Baremetalhost VM exist")"
 
+      #Verify the VMs interfaces
       BM_VM_IFACES="$(virsh domiflist "${BM_VMNAME}")"
       for bridge in ${BRIDGES}; do
         echo "$BM_VM_IFACES" | grep -w "${bridge}"  > /dev/null
         FAILS="$(process_status $? \
           "${name} Baremetalhost VM interface ${bridge} exist")"
       done
+
+      #Verify the instrospection completed successfully
+      FAILS="$(equals "$(echo "${BM_HOST}" | jq -r '.status.provisioning.state')" \
+        "ready" "${name} Baremetalhost introspecting completed")"
       echo "" >&3
     done
     echo "${FAILS}"
+    if [[ "${FAILS_CHECK}" != "${FAILS}" ]]; then
+      return 1
+    fi
+    return 0
 }
 
 
 #Verify that a resource exists in a type
 check_k8s_entity() {
+  local FAILS_CHECK="${FAILS}"
   for name in ${2}; do
     # Check entity exists
     ENTITY="$(kubectl --kubeconfig "${KUBECONFIG}" get "${1}" "${name}" -n metal3 -o json)"
@@ -111,11 +156,16 @@ check_k8s_entity() {
   done
   echo "" >&3
   echo "${FAILS}"
+  if [[ "${FAILS_CHECK}" != "${FAILS}" ]]; then
+    return 1
+  fi
+  return 0
 }
 
 
 #Verify that a resource exists in a type
 check_k8s_rs() {
+  local FAILS_CHECK="${FAILS}"
   for name in ${1}; do
     # Check entity exists
     ENTITY="$(kubectl --kubeconfig "${KUBECONFIG}" get replicasets -l name="${name}" -n metal3 -o json | jq '.items[0]')"
@@ -128,6 +178,18 @@ check_k8s_rs() {
   done
   echo "" >&3
   echo "${FAILS}"
+  if [[ "${FAILS_CHECK}" != "${FAILS}" ]]; then
+    return 1
+  fi
+  return 0
+}
+
+#Verify a container is running
+check_container(){
+  NAME="$1"
+  sudo "${CONTAINER_RUNTIME}" ps | grep -w "$NAME$" > /dev/null
+  process_status $? "Container ${NAME} running"
+  return $?
 }
 
 KUBECONFIG="${KUBECONFIG:-${HOME}/.kube/config}"
@@ -166,6 +228,16 @@ for name in ${EXPTD_CRDS}; do
 done
 echo "" >&3
 
+
+# Verify the Operators, stateful sets
+FAILS=$(iterate check_k8s_entity statefulsets "${EXPTD_STATEFULSETS}")
+
+# Verify the Operators, Deployments
+FAILS=$(iterate check_k8s_entity deployments "${EXPTD_DEPLOYMENTS}")
+
+# Verify the Operators, Replica sets
+FAILS=$(iterate check_k8s_rs "${EXPTD_DEPLOYMENTS}")
+
 # Verify the baremetal hosts
 ## Fetch the BM CRs
 BM_HOSTS="$(kubectl --kubeconfig "${KUBECONFIG}" get baremetalhosts -n metal3 -o json)"
@@ -174,22 +246,12 @@ FAILS=$(process_status $? "Fetch Baremetalhosts")
 BM_VMS="$(virsh list --all)"
 FAILS=$(process_status $? "Fetch Baremetalhosts VMs")
 ## Verify
-FAILS="$(list_nodes | check_bm_hosts)"
-
-# Verify the Operators, stateful sets
-FAILS=$(check_k8s_entity statefulsets "${EXPTD_STATEFULSETS}")
-
-# Verify the Operators, Deployments
-FAILS=$(check_k8s_entity deployments "${EXPTD_DEPLOYMENTS}")
-
-# Verify the Operators, Replica sets
-FAILS=$(check_k8s_rs "${EXPTD_DEPLOYMENTS}")
+FAILS="$(list_nodes | iterate check_bm_hosts)"
 
 
 #Verify Ironic containers are running
 for name in ironic ironic-inspector dnsmasq httpd mariadb; do
-  sudo "${CONTAINER_RUNTIME}" ps | grep -w "$name$" > /dev/null
-  FAILS=$(process_status $? "Container ${name} running")
+  FAILS="$(iterate check_container "${name}")"
 done
 echo "" >&3
 
