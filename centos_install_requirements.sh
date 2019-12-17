6fa8da77210feb2df83929fb5e73714763dfde00
Steven Hardy <shardy@redhat.com>
2019-12-17 05:55:32 -0500

    WIP enable CentOS8

--- centos_install_requirements.sh
+++ centos_install_requirements.sh
@@ -20,28 +20,26 @@ sudo yum -y update
 source /etc/os-release
 # VERSION_ID can be "7" or "8.x" so strip the minor version
 DISTRO="${ID}${VERSION_ID%.*}"
-if [ ! -f /etc/yum.repos.d/epel.repo ] ; then
-    if [[ $DISTRO == "rhel7" ]]; then
-        sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-    elif [[ $DISTRO == "centos7" ]]; then
-        sudo yum -y install epel-release --enablerepo=extras
-    fi
-fi
 
 if [[ $DISTRO == "centos7" ]]; then
-    sudo yum -y install epel-release dnf --enablerepo=extras
-fi
-
-if [[ $DISTRO == "rhel8" ]]; then
-    sudo subscription-manager repos --enable=ansible-2-for-rhel-8-x86_64-rpms
-    sudo dnf -y install python3
-    sudo alternatives --set python /usr/bin/python3
-else
+  sudo yum -y install epel-release dnf --enablerepo=extras
   # Install tripleo-repos, used to get a more recent version of some packages on CentOS7
   sudo dnf -y --repofrompath="current-tripleo,https://trunk.rdoproject.org/${DISTRO}-master/current-tripleo" install "python*-tripleo-repos" --nogpgcheck
   sudo tripleo-repos current-tripleo
   # There are some packages which are newer in the tripleo repos
   sudo yum -y update
+fi
+
+if [[ $DISTRO == "rhel8" ]]; then
+  sudo subscription-manager repos --enable=ansible-2-for-rhel-8-x86_64-rpms
+elif [[ $DISTRO == "centos8" ]]; then
+  sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+  sudo dnf -y install epel-release
+fi
+
+if [[ $DISTRO == "rhel8" ]] || [[ $DISTRO == "centos8" ]]; then
+  sudo dnf -y install python3
+  sudo alternatives --set python /usr/bin/python3
 fi
 
 # Install required packages
