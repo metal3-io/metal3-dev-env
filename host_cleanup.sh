818b36ceef4a11d7c005a379bf1918d807e8420d
Dhawal Patel <dhawal.patel@nordstrom.com>
2019-12-30 14:27:40 -0800

    Fix host_cleanup.sh to set num_nodes

--- host_cleanup.sh
+++ host_cleanup.sh
@@ -34,8 +34,7 @@ fi
 
 ANSIBLE_FORCE_COLOR=true ansible-playbook \
     -e "working_dir=$WORKING_DIR" \
-    -e "num_masters=$NUM_MASTERS" \
-    -e "num_workers=$NUM_WORKERS" \
+    -e "num_nodes=$NUM_NODES" \
     -e "extradisks=$VM_EXTRADISKS" \
     -e "virthost=$HOSTNAME" \
     -e "manage_baremetal=$MANAGE_BR_BRIDGE" \
