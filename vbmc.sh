15c3d6aa5c8e4aaf9b19aeafa705cb1c3b0a8792
Steven Hardy <shardy@redhat.com>
2020-02-14 09:55:07 +0000

    Make wrapper scripts work from any $PWD
    Currently these only work when run from inside the metal3-dev-env
    directory due to the relative path for the source of lib/common.sh
    but with this approach we should be able to run e.g "vbmc" from
    any $PWD.

--- vbmc.sh
+++ vbmc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-source lib/common.sh
+DIR="$(dirname "$(readlink -f "$0")")"
+# shellcheck source=lib/common.sh
+source "${DIR}/lib/common.sh"
 
 sudo "${CONTAINER_RUNTIME}" exec -ti vbmc vbmc "$@"
