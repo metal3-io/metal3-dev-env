34aa0a17e978d200736fae875f0ad612f89446e7
Your Name <you@example.com>
2020-05-22 12:47:45 +0000

    Add pivoting test ansible script

---
+++ scripts/v1alphaX/pivot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+METAL3_DIR="$(dirname "$(readlink -f "${0}")")/../.."
+
+# shellcheck disable=SC1091
+# shellcheck disable=SC1090
+source "${METAL3_DIR}/lib/common.sh"
+
+# shellcheck disable=SC1091
+# shellcheck disable=SC1090
+source "${METAL3_DIR}/lib/network.sh"
+
+# shellcheck disable=SC1091
+# shellcheck disable=SC1090
+source "${METAL3_DIR}/lib/images.sh"
+
+# Disable SSH strong authentication
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+ANSIBLE_FORCE_COLOR=true ansible-playbook \
+   -e "metal3_dir=$SCRIPTDIR" \
+   -e "v1aX_integration_test_action=pivoting" \
+   -i "${METAL3_DIR}/vm-setup/inventory.ini" \
+   -b -vvv "${METAL3_DIR}/vm-setup/v1aX_integration_test.yml"
