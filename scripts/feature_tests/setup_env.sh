21600ff54574c7283b4c332f33d04b53f3346f1c
kashifest <kashif.khan@est.tech>
2020-08-10 12:58:18 +0300

    Fix upgrade env vars export

--- scripts/feature_tests/setup_env.sh
+++ scripts/feature_tests/setup_env.sh
@@ -5,8 +5,10 @@ set -x
 M3PATH="$(dirname "$(readlink -f "${0}")")/../.."
 
 if [[ "${1}" == "ug" ]]; then
-  export CAPM3RELEASE="v0.3.2"
-  export CAPIRELEASE="v0.3.4"
+
+  # shellcheck disable=SC1091
+  # shellcheck source="$M3PATH/scripts/feature_tests/upgrade_vars.sh"
+  source "$M3PATH/scripts/feature_tests/upgrade_vars.sh"
 fi
 pushd "${M3PATH}" || exit
 make
