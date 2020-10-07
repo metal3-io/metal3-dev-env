1208a3922d18c2f2572754fb2863a4e906fcc6ea
kashifest <kashif.khan@est.tech>
2020-10-07 10:34:54 +0300

    Change erroneous Ubuntu Node Image

--- lib/images.sh
+++ lib/images.sh
@@ -3,7 +3,7 @@
 # Image url and checksum
 IMAGE_OS=${IMAGE_OS:-Centos}
 if [[ "${IMAGE_OS}" == "Ubuntu" ]]; then
-  export IMAGE_NAME=${IMAGE_NAME:-UBUNTU_20.04_NODE_IMAGE_K8S_v1.18.8.qcow2}
+  export IMAGE_NAME=${IMAGE_NAME:-UBUNTU_NODE_IMAGE_K8S_v1.18.8.qcow2}
   export IMAGE_LOCATION=${IMAGE_LOCATION:-https://artifactory.nordix.org/artifactory/airship/images/k8s_v1.18.8/}
 elif [[ "${IMAGE_OS}" == "FCOS" ]]; then
   export IMAGE_NAME=${IMAGE_NAME:-fedora-coreos-32.20200923.2.0-openstack.x86_64.qcow2.xz}
@@ -15,6 +15,7 @@ else
   export IMAGE_NAME=${IMAGE_NAME:-cirros-0.5.1-x86_64-disk.img}
   export IMAGE_LOCATION=${IMAGE_LOCATION:-http://download.cirros-cloud.net/0.5.1}
 fi
+
 export IMAGE_URL=http://$PROVISIONING_URL_HOST/images/${IMAGE_NAME}
 export IMAGE_CHECKSUM=http://$PROVISIONING_URL_HOST/images/${IMAGE_NAME}.md5sum
 
