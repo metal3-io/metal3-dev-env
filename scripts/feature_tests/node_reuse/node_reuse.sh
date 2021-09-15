cdfb5a7db72569f32bcfffbbf95af6b9714fde0b
smoshiur1237 <moshiur.rahman@est.tech>
2021-09-15 09:07:08 +0300

    Add upgraded kubernetes version to v1.22.1 for node reuse

--- scripts/feature_tests/node_reuse/node_reuse.sh
+++ scripts/feature_tests/node_reuse/node_reuse.sh
@@ -8,8 +8,8 @@ METAL3_DIR="$(dirname "$(readlink -f "${0}")")/../../.."
 source "${METAL3_DIR}/scripts/feature_tests/feature_test_vars.sh"
 
 export ACTION="node_reuse"
-export FROM_K8S_VERSION="v1.21.2"
+export FROM_K8S_VERSION="v1.22.0"
 export KUBERNETES_VERSION=${FROM_K8S_VERSION}
-export UPGRADED_K8S_VERSION="v1.22.0"
+export UPGRADED_K8S_VERSION="v1.22.1"
 
 "${METAL3_DIR}"/scripts/run.sh
