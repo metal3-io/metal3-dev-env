809ddf5c1c3358bd3a3445e470d83229ac050f3e
Anwar Hassen <anwar.hassen@est.tech>
2021-12-07 06:30:09 +0000

    Test getting capi and capm3 latest releases

--- scripts/feature_tests/upgrade/upgrade_vars.sh
+++ scripts/feature_tests/upgrade/upgrade_vars.sh
@@ -4,26 +4,17 @@ set -eux
 # Folder created for specific capi release when running
 # ${CLUSTER_API_REPO}/cmd/clusterctl/hack/create-local-repository.py
 
-export CAPIRELEASE_HARDCODED="v0.4.99"
-
-function get_latest_capm3_release() {
-    clusterctl upgrade plan | grep infrastructure-metal3 | awk 'NR == 1 {print $5}'
-}
 
 # CAPM3 release version which we upgrade from.
 export CAPM3RELEASE="v0.5.0"
-CAPM3_REL_TO_VERSION="$(get_latest_capm3_release)" || true
+CAPM3_REL_TO_VERSION="v0.5.1"
 # CAPM3 release version which we upgrade to.
 export CAPM3_REL_TO_VERSION
 
-# Fetch latest release version of CAPI from the output of clusterctl command.
-function get_latest_capi_release() {
-    clusterctl upgrade plan | grep cluster-api | awk 'NR == 1 {print $5}'
-}
-
 # CAPI release version which we upgrade from.
+export CAPIRELEASE_HARDCODED="v0.4.99"
 export CAPIRELEASE="v0.4.1"
-CAPI_REL_TO_VERSION="$(get_latest_capi_release)" || true
+CAPI_REL_TO_VERSION="v0.4.2"
 # CAPI release version which we upgrade to.
 export CAPI_REL_TO_VERSION
 export FROM_K8S_VERSION="v1.22.2"
