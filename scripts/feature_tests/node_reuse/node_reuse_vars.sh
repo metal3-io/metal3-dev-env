253c798ab802a3004ad8ff80b28a553d54d694bf
Lennart Jern <lennart.jern@est.tech>
2022-02-12 13:07:34 +0200

    Make IMAGE_OS case sensitive again
    This is the last step in changeing IMAGE_OS to take all lower case
    values like "ubuntu" and "centos" for CI runs. As a temporary solution
    while transitioning, both capitalized and lower case values were
    supported. With this change only lower case values will work.

--- scripts/feature_tests/node_reuse/node_reuse_vars.sh
+++ scripts/feature_tests/node_reuse/node_reuse_vars.sh
@@ -2,10 +2,7 @@
 
 export UPGRADED_K8S_VERSION="v1.23.3"
 
-# NOTE: ${VAR,,} is making the varable lower case.
-# This is temporary and will be removed once we have changed IMAGE_OS to be
-# lower case in the CI.
-if [[ "${IMAGE_OS,,}" == "ubuntu" ]]; then
+if [[ "${IMAGE_OS}" == "ubuntu" ]]; then
   export UPGRADED_IMAGE_NAME="UBUNTU_20.04_NODE_IMAGE_K8S_${UPGRADED_K8S_VERSION}.qcow2"
   export UPGRADED_RAW_IMAGE_NAME="UBUNTU_20.04_NODE_IMAGE_K8S_${UPGRADED_K8S_VERSION}-raw.img"
 else
