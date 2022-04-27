d1d2d0f64a0cefe641f3ac24fec6ad43a11d6ec7
Lennart Jern <lennart.jern@est.tech>
2022-04-27 17:55:12 +0300

    Avoid overiding working dir in playbooks
    Ansible overrides all variables in tasks when passing them as extra
    vars (e.g. -e my_var=my_val). The working_dir variable is already picked
    up from the environment and can be overridden in some tasks, so we
    should not set working_dir as extra var.

--- scripts/run.sh
+++ scripts/run.sh
@@ -30,12 +30,11 @@ source "${METAL3_DIR}/lib/ironic_basic_auth.sh"
 # Disable SSH strong authentication
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-# Ansible config file 
+# Ansible config file
 export ANSIBLE_CONFIG=${METAL3_DIR}/ansible.cfg
 
 ANSIBLE_FORCE_COLOR=true ansible-playbook \
    -e "metal3_dir=$SCRIPTDIR" \
    -e "v1aX_integration_test_action=${ACTION}" \
-   -e "working_dir=$WORKING_DIR" \
    -i "${METAL3_DIR}/vm-setup/inventory.ini" \
    -b -v "${METAL3_DIR}/vm-setup/v1aX_integration_test.yml"
