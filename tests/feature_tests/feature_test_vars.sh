9c2ff7275ed72012e8ea468bef3ca38412a53d3a
mboukhalfa <mohammed.boukhalfa@est.tech>
2022-09-09 14:43:59 +0300

    test ubuntu 22.04.1 with mac policy none

--- tests/feature_tests/feature_test_vars.sh
+++ tests/feature_tests/feature_test_vars.sh
@@ -6,6 +6,6 @@ if [ "${CAPM3RELEASEBRANCH}" == "release-0.5" ];
 then
   export FROM_K8S_VERSION="v1.23.5"
 else
-  export FROM_K8S_VERSION="v1.24.0"
+  export FROM_K8S_VERSION="v1.23.8"
 fi
 export KUBERNETES_VERSION="${FROM_K8S_VERSION}"
