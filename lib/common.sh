7500dd94b91b0d6aebb6e979275e76f9840de999
Huy Mai <huy.mai@est.tech>
2024-06-06 11:11:46 +0300

    Update bmo and tagalived tags for release-1.7
    Signed-off-by: Huy Mai <huy.mai@est.tech>

--- lib/common.sh
+++ lib/common.sh
@@ -279,8 +279,8 @@ elif [[ "${CAPM3RELEASEBRANCH}" = "release-1.6" ]]; then
 elif [[ "${CAPM3RELEASEBRANCH}" = "release-1.7" ]]; then
   export CAPM3_IMAGE=${CAPM3_IMAGE:-"${CONTAINER_REGISTRY}/metal3-io/cluster-api-provider-metal3:release-1.7"}
   export IPAM_IMAGE=${IPAM_IMAGE:-"${CONTAINER_REGISTRY}/metal3-io/ip-address-manager:release-1.7"}
-  export BARE_METAL_OPERATOR_TAG="v0.6.0"
-  export KEEPALIVED_TAG="v0.6.0"
+  export BARE_METAL_OPERATOR_TAG="v0.6.1"
+  export KEEPALIVED_TAG="v0.6.1"
   export IRONIC_TAG="v24.1.1"
   export BMOBRANCH="${BMORELEASEBRANCH:-release-0.6}"
 else
