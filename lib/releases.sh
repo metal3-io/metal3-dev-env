c00021ba5ff9df5e41b2c8ff4b6ee3d4693e6fe1
Muhammad Adil Ghaffar <muhammad.adil.ghaffar@est.tech>
2024-08-02 14:55:34 +0300

    bump k8s to v1.31.0
    Signed-off-by: Muhammad Adil Ghaffar <muhammad.adil.ghaffar@est.tech>

--- lib/releases.sh
+++ lib/releases.sh
@@ -84,7 +84,7 @@ elif [ "${CAPM3RELEASEBRANCH}" = "release-1.7" ]; then
 else
   # 1.7.99 points to the head of the main branch as well. Local override for CAPM3 is created for this version.
   export CAPM3RELEASE="v1.7.99"
-  export CAPIRELEASE="${CAPIRELEASE:-$(get_latest_release "${CAPIRELEASEPATH}" "v1.7.")}"
+  export CAPIRELEASE="${CAPIRELEASE:-$(get_latest_release "${CAPIRELEASEPATH}" "v1.8.")}"
 fi
 
 CAPIBRANCH="${CAPIBRANCH:-${CAPIRELEASE}}"
