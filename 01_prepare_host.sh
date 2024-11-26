f0a682db65316d1aa82933ef5aa2a02fa83e2ebf
Tuomo Tanskanen <tuomo.tanskanen@est.tech>
2024-11-26 13:54:38 +0200

    reformat 01 script bash syntax
    As part of dev-env bash cleanup patch series, cleanup 01 script.
    
    Signed-off-by: Tuomo Tanskanen <tuomo.tanskanen@est.tech>

--- 01_prepare_host.sh
+++ 01_prepare_host.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -eux
 
 # shellcheck disable=SC1091
@@ -6,44 +7,47 @@ source lib/logging.sh
 # shellcheck disable=SC1091
 source lib/common.sh
 
-if [[ "$(id -u)" -eq 0 ]]; then
-  echo "Please run 'make' as a non-root user"
-  exit 1
+if [[ "${EUID}" -eq 0 ]]; then
+    echo "Please run 'make' as a non-root user"
+    exit 1
 fi
 
 if [[ "${OS}" = "ubuntu" ]]; then
-  # Set apt retry limit to higher than default to
-  # make the data retrival more reliable
-  sudo sh -c ' echo "Acquire::Retries \"10\";" > /etc/apt/apt.conf.d/80-retries '
-  sudo apt-get update
-  sudo apt-get -y install python3-pip python3-dev python3-venv jq curl wget pkg-config bash-completion
+    # Set apt retry limit to higher than default to
+    # make the data retrival more reliable
+    sudo sh -c ' echo "Acquire::Retries \"10\";" > /etc/apt/apt.conf.d/80-retries '
+    sudo apt-get update
+    sudo apt-get -y install python3-pip python3-dev python3-venv jq curl wget pkg-config bash-completion
 
-  # Set update-alternatives to python3
-  if [[ "${DISTRO}" = "ubuntu20" ]]; then
-    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
-  elif [[ "${DISTRO}" = "ubuntu22" ]]; then
-    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
-  elif [[ "${DISTRO}" = "ubuntu24" ]]; then
-    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.12 1
-  fi
-elif [[ "${OS}" = "centos" ]] || [[ "${OS}" = "rhel" ]]; then
-  sudo dnf upgrade -y --nobest
-  case "${VERSION_ID}" in
-    8)
-      sudo dnf config-manager --set-enabled powertools
-      sudo dnf install -y epel-release
-      ;;
-    9)
-      sudo dnf config-manager --set-enabled crb
-      sudo dnf install -y "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
-      ;;
-    *)
-      echo -n "CentOS or RHEL version not supported"
-      exit 1
-      ;;
+    # Set update-alternatives to python3
+    case "${DISTRO}" in
+        ubuntu20)
+            sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1 ;;
+        ubuntu22)
+            sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1 ;;
+        ubuntu24)
+            sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.12 1 ;;
+        *) ;;
   esac
-  sudo dnf -y install python3-pip jq curl wget pkgconf-pkg-config bash-completion
-  sudo ln -s /usr/bin/python3 /usr/bin/python || true
+
+elif [[ "${OS}" = "centos" ]] || [[ "${OS}" = "rhel" ]]; then
+    sudo dnf upgrade -y --nobest
+    case "${VERSION_ID}" in
+        8)
+            sudo dnf config-manager --set-enabled powertools
+            sudo dnf install -y epel-release
+            ;;
+        9)
+            sudo dnf config-manager --set-enabled crb
+            sudo dnf install -y "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
+            ;;
+        *)
+            echo -n "CentOS or RHEL version not supported"
+            exit 1
+            ;;
+    esac
+    sudo dnf -y install python3-pip jq curl wget pkgconf-pkg-config bash-completion
+    sudo ln -s /usr/bin/python3 /usr/bin/python || true
 fi
 
 # NOTE(tuminoid) lib/releases.sh must be after the jq and python installation
@@ -71,60 +75,64 @@ sudo "${ANSIBLE_VENV}/bin/pip" install --ignore-installed ansible=="${ANSIBLE_VE
 
 # Install required packages
 ANSIBLE_FORCE_COLOR=true "${ANSIBLE}-playbook" \
-  -e "working_dir=${WORKING_DIR}" \
-  -e "metal3_dir=${SCRIPTDIR}" \
-  -e "virthost=${HOSTNAME}" \
-  -i vm-setup/inventory.ini \
-  -b vm-setup/install-package-playbook.yml
+    -e "working_dir=${WORKING_DIR}" \
+    -e "metal3_dir=${SCRIPTDIR}" \
+    -e "virthost=${HOSTNAME}" \
+    -i vm-setup/inventory.ini \
+    -b vm-setup/install-package-playbook.yml
 
 # Add usr/local/go/bin to the PATH environment variable
 GOBINARY="${GOBINARY:-/usr/local/go/bin}"
 if [[ ! "${PATH}" =~ .*(:|^)(${GOBINARY})(:|$).* ]]; then
-  echo "export PATH=${PATH}:${GOBINARY}" >> ~/.bashrc
-  export PATH=${PATH}:${GOBINARY}
+    echo "export PATH=${PATH}:${GOBINARY}" >> ~/.bashrc
+    export PATH=${PATH}:${GOBINARY}
 fi
-
 
 ## Install krew
 if ! kubectl krew > /dev/null 2>&1; then
-  download_and_install_krew
+    download_and_install_krew
 fi
 
 # Allow local non-root-user access to libvirt
+# shellcheck disable=SC2312
 if ! id "${USER}" | grep -q libvirt; then
-  sudo usermod -a -G "libvirt" "${USER}"
+    sudo usermod -a -G "libvirt" "${USER}"
 fi
 
 if [[ "${EPHEMERAL_CLUSTER}" = "minikube" ]]; then
-  if ! command -v minikube &>/dev/null || [[ "$(minikube version --short)" != "${MINIKUBE_VERSION}" ]]; then
-    download_and_install_minikube
-    download_and_install_kvm2_driver
-  fi
+    # shellcheck disable=SC2312
+    if ! command -v minikube &>/dev/null || [[ "$(minikube version --short)" != "${MINIKUBE_VERSION}" ]]; then
+        download_and_install_minikube
+        download_and_install_kvm2_driver
+    fi
 
-  if ! command -v docker-machine-driver-kvm2 &>/dev/null ; then
-    download_and_install_kvm2_driver
-  fi
+    if ! command -v docker-machine-driver-kvm2 &>/dev/null ; then
+        download_and_install_kvm2_driver
+    fi
 # Install Kind for both Kind and tilt
 else
-  if ! command -v kind &>/dev/null || [[ "v$(kind version -q)" != "${KIND_VERSION}" ]]; then
-    download_and_install_kind
-  fi
-  if [[ "${EPHEMERAL_CLUSTER}" = "tilt" ]]; then
-    download_and_install_tilt
-  fi
+    # shellcheck disable=SC2312
+    if ! command -v kind &>/dev/null || [[ "v$(kind version -q)" != "${KIND_VERSION}" ]]; then
+        download_and_install_kind
+    fi
+    if [[ "${EPHEMERAL_CLUSTER}" = "tilt" ]]; then
+        download_and_install_tilt
+    fi
 fi
 
+# shellcheck disable=SC2312
 if ! command -v kubectl &>/dev/null || [[ "$(kubectl version --client -o json|jq -r '.clientVersion.gitVersion')" != "${KUBECTL_VERSION}" ]]; then
-  download_and_install_kubectl
+    download_and_install_kubectl
 fi
 
 if ! command -v kustomize &>/dev/null; then
-  download_and_install_kustomize
+    download_and_install_kustomize
 fi
 
 BASH_COMPLETION="/etc/bash_completion.d/kubectl"
 if [[ ! -r "${BASH_COMPLETION}" ]]; then
-  kubectl completion bash | sudo tee "${BASH_COMPLETION}"
+    # shellcheck disable=SC2312
+    kubectl completion bash | sudo tee "${BASH_COMPLETION}"
 fi
 
 # Clean-up any old ironic containers
@@ -132,22 +140,23 @@ remove_ironic_containers
 
 # Clean-up existing pod, if podman
 case "${CONTAINER_RUNTIME}" in
-  podman)
-    for pod in ironic-pod infra-pod; do
-      if  sudo "${CONTAINER_RUNTIME}" pod exists "${pod}" ; then
-          sudo "${CONTAINER_RUNTIME}" pod rm "${pod}" -f
-      fi
-      sudo "${CONTAINER_RUNTIME}" pod create -n "${pod}"
-    done
-    ;;
-  *)
-    ;;
+    podman)
+        for pod in ironic-pod infra-pod; do
+            if  sudo "${CONTAINER_RUNTIME}" pod exists "${pod}" ; then
+                sudo "${CONTAINER_RUNTIME}" pod rm "${pod}" -f
+            fi
+            sudo "${CONTAINER_RUNTIME}" pod create -n "${pod}"
+        done
+        ;;
+    *)
+        ;;
 esac
+
 # TODO (mboukhalfa) fake images
-if [[ "${NODES_PLATFORM}" == "fake" ]]; then
-  echo "Skipping image prepulling on fake nodes platform"
+if [[ "${NODES_PLATFORM}" = "fake" ]]; then
+    echo "Skipping image prepulling on fake nodes platform"
 else
-  # pre-pull node and container images
-  # shellcheck disable=SC1091
-  source lib/image_prepull.sh
+    # pre-pull node and container images
+    # shellcheck disable=SC1091
+    source lib/image_prepull.sh
 fi
