5b67e53da87494a162643778eeacd3a6a7547f3c
Muhammad Adil Ghaffar <muhammad.adil.ghaffar@est.tech>
2025-08-28 11:32:32 +0300

    bump k8s to 1.34
    Signed-off-by: Muhammad Adil Ghaffar <muhammad.adil.ghaffar@est.tech>

--- config_example.sh
+++ config_example.sh
@@ -133,8 +133,8 @@
 #
 #export CAPI_VERSION=v1beta2
 
-#export KUBERNETES_VERSION="v1.33.0"
-#export UPGRADED_K8S_VERSION="v1.33.0"
+#export KUBERNETES_VERSION="v1.34.0"
+#export UPGRADED_K8S_VERSION="v1.34.0"
 
 # Version of kubelet, kubeadm and kubectl binaries
 #export KUBERNETES_BINARIES_VERSION="${KUBERNETES_BINARIES_VERSION:-${KUBERNETES_VERSION}}"
@@ -149,11 +149,11 @@
 
 # Image for target hosts deployment
 #
-#export IMAGE_NAME="CENTOS_9_NODE_IMAGE_K8S_v1.33.0.qcow2"
+#export IMAGE_NAME="CENTOS_9_NODE_IMAGE_K8S_v1.34.0.qcow2"
 
 # Location of the image to download
 #
-#export IMAGE_LOCATION="https://artifactory.nordix.org/artifactory/metal3/images/k8s_v1.33.0"
+#export IMAGE_LOCATION="https://artifactory.nordix.org/artifactory/metal3/images/k8s_v1.34.0"
 
 # Image username for ssh
 #
