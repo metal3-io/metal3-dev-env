496bdf46875fec4388f22c9711790eb7ab3c69e6
Dmitry Tantsur <dtantsur@protonmail.com>
2026-08-11 12:29:02 +0200

    Clean up Ironic credentials handling
    1. Do not assume that IRONIC_AUTH_DIR has a trailing slash
    2. Use shared variables for username and password paths
    3. Chmod 0600 for the generated password file
    
    Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

--- lib/ironic_basic_auth.sh
+++ lib/ironic_basic_auth.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 # Create usernames and passwords and other files related to basic auth
-IRONIC_AUTH_DIR="${IRONIC_AUTH_DIR:-"${IRONIC_DATA_DIR}/auth/"}"
+IRONIC_AUTH_DIR="${IRONIC_AUTH_DIR:-"${IRONIC_DATA_DIR}/auth"}"
 mkdir -p "${IRONIC_AUTH_DIR}"
+
+IRONIC_USERNAME_FILE="${IRONIC_AUTH_DIR}/ironic-username"
+IRONIC_PASSWORD_FILE="${IRONIC_AUTH_DIR}/ironic-password"
 
 # If usernames and passwords are unset, read them from file or generate them
 if [ -z "${IRONIC_USERNAME:-}" ]; then
-    if [ ! -f "${IRONIC_AUTH_DIR}ironic-username" ]; then
+    if [ ! -f "${IRONIC_USERNAME_FILE}" ]; then
         IRONIC_USERNAME="$(uuidgen)"
-        echo -n "$IRONIC_USERNAME" > "${IRONIC_AUTH_DIR}ironic-username"
+        echo -n "$IRONIC_USERNAME" > "${IRONIC_USERNAME_FILE}"
     else
-        IRONIC_USERNAME="$(cat "${IRONIC_AUTH_DIR}ironic-username")"
+        IRONIC_USERNAME="$(cat "${IRONIC_USERNAME_FILE}")"
     fi
 fi
 if [ -z "${IRONIC_PASSWORD:-}" ]; then
-    if [ ! -f "${IRONIC_AUTH_DIR}ironic-password" ]; then
+    if [ ! -f "${IRONIC_PASSWORD_FILE}" ]; then
         IRONIC_PASSWORD="$(uuidgen)"
-        echo -n "$IRONIC_PASSWORD" > "${IRONIC_AUTH_DIR}ironic-password"
+        echo -n "$IRONIC_PASSWORD" > "${IRONIC_PASSWORD_FILE}"
+        chmod 0600 "${IRONIC_PASSWORD_FILE}"
     else
-        IRONIC_PASSWORD="$(cat "${IRONIC_AUTH_DIR}ironic-password")"
+        IRONIC_PASSWORD="$(cat "${IRONIC_PASSWORD_FILE}")"
     fi
 fi
 
